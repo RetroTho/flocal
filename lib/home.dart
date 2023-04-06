5d8fc24e0e9e620ea1a2f1648558714763a5fb77
RetroTho <74274119+RetroTho@users.noreply.github.com>
2023-04-05 17:20:44 -0700

    make HomePage class stateful

--- lib/home.dart
+++ lib/home.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key, required this.title});
   final String title;
+  
+  @override
+  State<StatefulWidget> createState() => _HomePage();
+}
 
+class _HomePage extends State<HomePage> {
   @override
   Widget build(BuildContext context){
     return const Scaffold(
