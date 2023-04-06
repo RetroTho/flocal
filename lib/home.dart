146f29ba981120decbb690b158295b3e8a61942f
RetroTho <74274119+RetroTho@users.noreply.github.com>
2023-04-05 17:35:03 -0700

    implement basic timer

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key, required this.title});
@@ -9,11 +10,39 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePage extends State<HomePage> {
+  Timer _timer = Timer(const Duration(), () {});
+  int _time = 10;
+
+  void startTimer() {
+    const oneSec = Duration(seconds: 1);
+    _timer = Timer.periodic(
+      oneSec,
+      (Timer timer) {
+        if (_time == 0){
+          setState(() {
+            timer.cancel();
+          });
+        }
+        else {
+          setState(() {
+            _time--;
+          });
+        }
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context){
-    return const Scaffold(
-      body: Center(
-        child: Text('Hello World!'),
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          Text("$_time"),
+          ElevatedButton(onPressed: () {
+            startTimer();
+          },
+          child: const Text("Start")),
+        ],
       ),
     );
   }
